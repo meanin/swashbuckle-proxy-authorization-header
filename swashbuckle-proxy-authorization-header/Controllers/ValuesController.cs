a5f66bd82291c5b0eb28e5b34304a79f0e8f7325
Paweł Ruciński <pawel.rucinski@cybercom.com>
2017-11-28 16:28:32 +0100

    Added required proxy-authorization header in swagger ui

--- swashbuckle-proxy-authorization-header/Controllers/ValuesController.cs
+++ swashbuckle-proxy-authorization-header/Controllers/ValuesController.cs
@@ -1,52 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net;
 using System.Web.Http;
 using Swashbuckle.Swagger.Annotations;
 
 namespace swashbuckle_proxy_authorization_header.Controllers
 {
+    /// <summary>
+    /// Values controller
+    /// </summary>
     public class ValuesController : ApiController
     {
-        // GET api/values
-        [SwaggerOperation("GetAll")]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
+        /// <summary>
+        /// Get by id method
+        /// </summary>
+        /// <remarks>
+        /// Get string "value {id:int}" by called id
+        /// </remarks>
+        /// <param name="id">get id to return</param>
+        /// <returns>value with id</returns>
         [SwaggerOperation("GetById")]
         [SwaggerResponse(HttpStatusCode.OK)]
         [SwaggerResponse(HttpStatusCode.NotFound)]
         public string Get(int id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [SwaggerOperation("Create")]
-        [SwaggerResponse(HttpStatusCode.Created)]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [SwaggerOperation("Update")]
-        [SwaggerResponse(HttpStatusCode.OK)]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [SwaggerOperation("Delete")]
-        [SwaggerResponse(HttpStatusCode.OK)]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        public void Delete(int id)
-        {
+            return $"value {id}";
         }
     }
 }
